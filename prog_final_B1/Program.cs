c3ec355587f6f331c1b0691c11bdf75ebf901b4b
Ivan Kalinin <ivanopt1@ya.ru>
2022-12-26 21:13:47 +0700

    final version 1.0

--- prog_final_B1/Program.cs
+++ prog_final_B1/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+Clear();
+
+string[] array = myArray();
+string[] resArray = GetArray(array, 3);
+WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", resArray)}]");
+
+string[] GetArray(string[] ar, int n) {
+    string[] ar2 = new string[NewCound(ar, n)];
+
+    for(int i = 0, j = 0; i < ar.Length; i++) {
+        if(ar[i].Length <= n) {
+            ar2[j] = ar[i];
+            j++;
+        }
+    }
+
+    return ar2;
+}
+
+int NewCound(string[] ar, int n) {
+    int count = 0;
+
+    for(int i = 0; i < ar.Length; i++) {
+        if(ar[i].Length <= n) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+string[] myArray() {
+    Write("Введите значения через пробел: ");
+    return ReadLine().Split(" ");
+}
